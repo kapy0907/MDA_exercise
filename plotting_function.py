cb58bc9d7c65f728875faeb3288052ef1647cad1
Perny  Katharina (a01400416@unet.univie.ac.at) <a1400416@srvx1.img.univie.ac.at>
2022-11-30 15:59:13 +0100

    inital commit of temperature plotting function to compare 4 models

---
+++ plotting_function.py
@@ -0,0 +1,60 @@
+import numpy as np
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt 
+
+def plot_temperature_4_models(data, vmin=-15.0, vmax=15.0,
+                                 cmap='coolwarm', projection=ccrs.Mercator()):
+    """
+    Plotting routine for comparison of four models. 
+    Parameters
+    ----------
+    data : dict
+        keys = model name,
+        values = 2D temperature in °C with dimensions lon, lat
+    vmin, vmax (optional) : float 
+        limits for temperature range to make plots comparable
+        default: vmin=-15.0, vmax=15.0
+    cmap : colormap object or matplotlib colormap name
+        colormap for plotting
+    projection : cartopy projection
+        Cartopy projection object containing all the projection information of
+        the data
+        
+    Returns:
+    --------
+    None. 
+    
+    """
+
+    plt.close()
+    fig, axes  = plt.subplots(2,2, subplot_kw={'projection': ccrs.Mercator()})
+    [[ax1, ax2], [ax3, ax4]] = axes
+
+    model_axes = [ax1, ax2, ax3, ax4]
+
+    for model, model_ax in zip(data.keys(),model_axes):
+
+        bias = str(data[model].mean().values.round(2))
+        rmse = str(np.sqrt((data[model]**2).mean()).values.round(2))
+
+        im = model_ax.pcolormesh(data[model].lon, data[model].lat,
+                                data[model], vmin=-15, vmax=15,
+                                cmap='coolwarm',
+                                transform = ccrs.PlateCarree(),
+                               )
+        model_ax.axes.coastlines()
+        model_ax.set_title(model)
+
+        model_ax.text(0.027,0.047,
+                     'RMSE: ' + rmse + ' \nBIAS: ' + bias,
+                     bbox = dict(facecolor='white', alpha=0.8),
+                     transform=model_ax.transAxes,
+                    )
+
+    fig.subplots_adjust(bottom=0.1, top=0.95, left=0.1, right=0.8,
+                        wspace=0.1, hspace=0.1)
+
+    cb_ax = fig.add_axes([0.83, 0.11, 0.02, 0.83])
+    cbar = fig.colorbar(im, cax=cb_ax)
+
+    plt.show()
